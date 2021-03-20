bffc63fa0752ea21291469aba9cd98ad21cad678
Camden Whirl <camdenwhirl@gmail.com>
2021-03-20 15:04:30 -0600

    Started the logic behind saving values into local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 var currentTime = getCurrentTime();
 timeBlockArray = [$('#0'), $('#1'), $('#2'), $('#3'), $('#4'), $('#5'), $('#6'), $('#7'), $('#8')];
 
-
+//sets current time to the 24 hour format
 function getCurrentTime () {
     var currentHour = parseInt(moment().format("hh"));
     var currentPeriod = moment().format("A");
@@ -12,6 +12,7 @@ function getCurrentTime () {
     return currentHour;
 }
 
+//fills in the background color of the timeblocks
 function setColors () {
     for (const block in timeBlockArray) {
         if (currentTime > $(`#${block}`).data("hour")) {
@@ -26,5 +27,8 @@ function setColors () {
 
 //sets time at top of screen
 $('#currentDay').text(moment());
-
 setColors();
+
+$('.table').siblings("button").on('click', function () {
+    console.log("Hello");
+});
