985461033726f92414a827879dc2071af8c7c86b
Camden Whirl <camdenwhirl@gmail.com>
2021-03-20 14:40:55 -0600

    Added the logic behind the timeblock coloring

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,30 @@
-$('#currentDay').text(moment());
-
-var currentHour = parseInt(moment().format("hh"));
-
+var currentTime = getCurrentTime();
 timeBlockArray = [$('#0'), $('#1'), $('#2'), $('#3'), $('#4'), $('#5'), $('#6'), $('#7'), $('#8')];
 
 
+function getCurrentTime () {
+    var currentHour = parseInt(moment().format("hh"));
+    var currentPeriod = moment().format("A");
+
+    if (currentPeriod == "PM"){
+        currentHour = currentHour + 12;
+    }
+    return currentHour;
+}
+
 function setColors () {
     for (const block in timeBlockArray) {
-        
+        if (currentTime > $(`#${block}`).data("hour")) {
+            $(`#${block}`).addClass("past");
+        } else if (currentTime < $(`#${block}`).data("hour")) {
+            $(`#${block}`).addClass("future");
+        } else {
+            $(`#${block}`).addClass("present");
+        }
     }
 }
+
+//sets time at top of screen
+$('#currentDay').text(moment());
 
 setColors();
