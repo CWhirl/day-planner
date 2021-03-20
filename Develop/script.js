4ed92b29185735efb9e9f3c0e84e95880920bb90
Camden Whirl <camdenwhirl@gmail.com>
2021-03-20 13:59:02 -0600

    Added id and data attributes to the timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,14 @@
 $('#currentDay').text(moment());
 
+var currentHour = parseInt(moment().format("hh"));
+
+timeBlockArray = [$('#0'), $('#1'), $('#2'), $('#3'), $('#4'), $('#5'), $('#6'), $('#7'), $('#8')];
+
+
+function setColors () {
+    for (const block in timeBlockArray) {
+        
+    }
+}
+
+setColors();
